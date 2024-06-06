8564c007bbb613331ceb6f8e93594ac448340110
unknown <mpmotta@gmail.com>
2024-06-06 11:21:27 -0300

    bugfix/valor-notas

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ nome = input("Insira o nome do aluno: ")
 # Captura as 4 notas do aluno
 notas = []
 for i in range(4):
-    nota = float(input(f"Insira a nota {i+1} do aluno: "))
-    notas.append(nota)
+    while True:
+        nota = float(input(f"Insira a nota {i+1} do aluno: "))
+        if 0 <= nota <= 10:
+            notas.append(nota)
+            break
+        else:
+            print("Nota inválida. Por favor, insira um número entre 0 e 10.")
 
 # Calcula a média das notas
 media = sum(notas) / len(notas)
 
 # Determina se o aluno está aprovado ou reprovado
 if media >= 7:
-    print(f"O(a) aluno(a) {nome} está aprovado(a) com média {media:.1f}.")
+    print(f"O aluno {nome} está aprovado com média {media:.1f}.")
 else:
-    print(f"O(a) aluno(a) {nome} está reprovado(a) com média {media:.1f}.")
+    print(f"O aluno {nome} está reprovado com média {media:.2f}.")
